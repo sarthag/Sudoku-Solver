6fe98cee786725eaae176ad3793d1c51d4a22010
Sarthak Agarwal <74304695+sarthag@users.noreply.github.com>
2021-06-06 02:52:01 +0530

    updated tester

--- test_solver.py
+++ test_solver.py
@@ -15,16 +15,16 @@ unsolved = np.array([[0, 0, 4, 3, 0, 0, 2, 0, 9],
                      [0, 4, 2, 9, 1, 0, 3, 0, 0]])
 
 unsolved = unsolved.astype(str)
+#solve(unsolved)
 
-"""
-str1 = "abc"
-str2 = str1 + "d"
-str3 = "123456789"
-print(str3.replace("5", ""))
-print(str3.replace("a", ""))
-print("5" in ["123", "45"])
-"""
-
-print_grid(get_dummies(unsolved))
+grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+unsolved2 = [i for i in grid2]
+for i in range(81):
+    if unsolved2[i] == ".":
+        unsolved2[i] ="0"
 
 
+unsolved2 = np.int_(unsolved2).reshape((9,9))  
+unsolved2 = unsolved2.astype(str)
+
+#solve(unsolved)
