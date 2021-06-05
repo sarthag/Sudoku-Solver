fcfb0cb1273e8096bb5beef836f06884924684cd
Sarthak Agarwal <74304695+sarthag@users.noreply.github.com>
2021-06-05 17:01:58 +0530

    updated tester

--- test_solver.py
+++ test_solver.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd 
 from solver import *
+from print_grid import print_grid
 
 
 unsolved = np.array([[0, 0, 4, 3, 0, 0, 2, 0, 9],
@@ -12,4 +13,18 @@ unsolved = np.array([[0, 0, 4, 3, 0, 0, 2, 0, 9],
                      [6, 0, 0, 0, 0, 0, 1, 0, 5],
                      [0, 0, 3, 5, 0, 8, 6, 9, 0],
                      [0, 4, 2, 9, 1, 0, 3, 0, 0]])
+
+unsolved = unsolved.astype(str)
+
+"""
+str1 = "abc"
+str2 = str1 + "d"
+str3 = "123456789"
+print(str3.replace("5", ""))
+print(str3.replace("a", ""))
+print("5" in ["123", "45"])
+"""
+
+print_grid(get_dummies(unsolved))
+
 
