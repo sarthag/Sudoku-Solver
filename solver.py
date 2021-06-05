9fc317c6164f844c260d4096ccb0e779811b76b0
Sarthak Agarwal <74304695+sarthag@users.noreply.github.com>
2021-06-05 17:02:21 +0530

    updated solver, added dummies

--- solver.py
+++ solver.py
@@ -10,7 +10,7 @@ def box_info(row, col):
   box_no = int((row/3))*3 + int(col/3)
   index_in_box = (row%3)*3 + col%3
   
-  return box_no , index_in_box
+  return box_no , index_in_box 
 
 
 def get_parts(puzzle):
@@ -20,13 +20,44 @@ def get_parts(puzzle):
   
   rows = puzzle
   cols = puzzle.T
-  boxes = np.zeros((9,9))
+  boxes = np.zeros((9,9)).astype(str)
 
   for i in range(9):
     for j in range(9):
       box_no, pos = box_info(i, j)
       boxes[box_no][pos] = puzzle[i][j]
-  boxes = np.int_(boxes)
+
 
   return rows, cols, boxes
 
+
+def update_puzzle(row, col, digit):
+  pass
+
+
+def get_dummies(puzzle):
+
+  #returns an array with the possible entries in each box
+  puzzle, puzzle_cols, puzzle_box = get_parts(puzzle)  
+  dummy_puzzle = puzzle.copy()  
+  
+  for i in range(9):
+    for j in range(9):
+      if dummy_puzzle[i][j] != "0":
+        continue
+      
+      box_no, box_ind = box_info(i, j)
+      possible = "123456789"
+      
+      for k in range(9):
+        possible = possible.replace(puzzle[i][k], "")
+        possible = possible.replace(puzzle_cols[j][k], "")
+        possible = possible.replace(puzzle_box[box_no][k], "")
+        if len(possible) == 1: break
+    
+      dummy_puzzle[i][j] = possible
+      if len(possible) == 1: update_puzzle(i, j, possible)
+      
+  return dummy_puzzle
+       
+  
