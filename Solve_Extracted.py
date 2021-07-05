097d9e2b06d491d3e86c78a773eb1ae1ea825ebb
Sarthak Agarwal <74304695+sarthag@users.noreply.github.com>
2021-07-05 19:05:12 +0530

    Added final Image

--- Solve_Extracted.py
+++ Solve_Extracted.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 from  Extract_Digits import *
 from Solver.solver import *
 from Solver.print_grid import *
@@ -10,6 +11,28 @@ def modified_where(array):
     indexes = np.column_stack((arr[0], arr[1]))
     return indexes
     
+    
+def place_digits(solved):
+    
+    final_image = np.ones((360, 360, 3), dtype = "uint8")*255
+    curr = 0 
+    for i in range(1, 9):
+        curr += 40
+        
+        if i%3 == 0: thickness = 4
+        else: thickness = 2
+        
+        cv2.line(final_image, (0, curr), (379, curr), (0, 0, 0), thickness)
+        cv2.line(final_image, (curr, 0), (curr, 379), (0, 0, 0), thickness)
+        
+        
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    for i in range(9):
+        for j in range(9):
+            cv2.putText(final_image, solved[i][j] ,(i*40 + 10, (j+1)*40 - 10), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
+    
+    return final_image
+
 
 
 def solve_sudoku(sudoku_image_path):
@@ -18,7 +41,7 @@ def solve_sudoku(sudoku_image_path):
     model2 = "model_dumps/model2.joblib"
     model3 = "model_dumps/model3.joblib"
     
-    unsolved1 = extract_digit_values(sudoku_image_path, model1)
+    unsolved1 = extract_digit_values(sudoku_image_path, model1) 
     solved, solved_sudoku = solve(unsolved1.copy())
     if solved: return solved_sudoku
     
